92cb08fda06e98388e58b9252e0996e19595d0da
trantrongnhanttn <trantrongnhanttn@gmail.com>
2019-10-16 21:54:21 +0700

    added background image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,30 @@
 import React from "react"
-// import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import { FaGulp } from "react-icons/fa"
-const IndexPage = () => (
+import BackgroundSection from "../components/Globals/BackgroundSection"
+
+const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <FaGulp></FaGulp>
+    <BackgroundSection
+      img={data.img.childImageSharp.fluid}
+      title="regular joe's"
+    ></BackgroundSection>
   </Layout>
 )
+
+export const query = graphql`
+  {
+    img: file(relativePath: { eq: "default-background.jpeg" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
