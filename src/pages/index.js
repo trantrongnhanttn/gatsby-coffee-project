e5f2f2fd15b2ad593c34245babe678805d9ff581
Trong Nhan <trantrongnhanttn@gmail.com>
2019-10-17 16:58:16 +0700

    our menu updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import BackgroundSection from "../components/Globals/BackgroundSection"
 import Info from "../components/Home/Info"
+import Menu from '../components/Home/Menu'
 
 const IndexPage = ({ data }) => (
   <Layout>
@@ -14,6 +15,7 @@ const IndexPage = ({ data }) => (
       title="regular joe's"
     ></BackgroundSection>
     <Info />
+    <Menu items={data.menu} />
   </Layout>
 )
 
@@ -26,7 +28,25 @@ export const query = graphql`
         }
       }
     }
-  }
+    menu: allContentfulCoffeeItem {
+      edges {
+        node {
+          id
+          title
+          price
+          description {
+            description
+          }
+          category
+          image {
+            fixed(width: 50, height: 50) {
+              ...GatsbyContentfulFixed_tracedSVG
+            }
+          }
+        }
+      }
+    }
+  }  
 `
 
 export default IndexPage
