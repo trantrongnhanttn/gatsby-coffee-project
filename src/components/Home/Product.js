ca73f1412526fc1cb9e756b2d9d762e351eb9af5
Trong Nhan <trantrongnhanttn@gmail.com>
2019-10-22 17:34:47 +0700

    update snipcart

--- src/components/Home/Product.js
+++ src/components/Home/Product.js
@@ -12,7 +12,7 @@ export default function Product({ product }) {
                 <button className="btn btn-yellow mt-3 text-capitalize snipcart-add-item"
                     data-item-id={product.id}
                     data-item-price={product.price}
-                    data-item-url="https://coffee-shop-devtato.netlify.com"
+                    data-item-url="https://coffee-shop-devtato.netlify.com/"
                     data-item-image={product.image.fluid.src}
                     data-item-name={product.title}>
                     add to cart
